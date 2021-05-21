458e21737a296fd9f7055c663d270eb95723903b
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-20 23:01:24 -0600

    birthday app finished

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import List from './List';
 
 function App() {
 
-  const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState(data);// The "people" from the array of objects in the  "data" component.  These "people"are passed in as a prop to the List component.
 
   return (
     <main>
     <section className="container">
       <h3>{people.length} birthdays today</h3>
       <List people={people}/>
-      <button onClick={() => console.log('click me')}>clear all</button>
+      <button onClick={() => setPeople([])}>clear all</button>
     </section>
   </main>
     )
--- src/List.js
+++ src/List.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const List = () => {
-  return (
+const List = ({ people }) => {// Taking the "people" objects from the "data" component as declared in the useState method.  
+  return (//  We the map over the list of people, using the id, name, age, and image properties from their respective objects, and creating an article element that displays all of those properties and how old the person is.
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        const {id, name, age, image} = person;
+        return <article key={id} className="person">
+          <img src="{image}" alt="{name}"/>
+          <div>
+            <h4>{name}</h4>
+            <p>{age} years</p>
+          </div>
+        </article>
+      })}
     </>
   );
 };

6d47bdfff2102bb44e4505dbba4cf124cd4d9500
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-23 09:36:57 -0600

    review app - now able to cycle through reviews with the next and previous buttons

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -6,17 +6,28 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const {name,job,image,text} = people[index];
 
+  const checkNumber = (number) => {
+    if(number > people.length - 1) {
+      return 0;
+    } else {
+      if(number < 0) {
+        return people.length - 1;
+      }
+      return number;
+    }
+  }
+
   const nextPerson = () => {
     setIndex((index) => {
       let newIndex = index + 1;
-      return newIndex;
+      return checkNumber(newIndex);
     })
   }
 
   const prevPerson = () => {
     setIndex((index) => {
       let newIndex = index - 1;
-      return newIndex;
+      return checkNumber(newIndex);
     })
   }
   return (

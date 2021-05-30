61a9b03a9183b9f930e34e14c937c908bd84d40f
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-29 18:12:12 -0600

    starting menu tab project

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,8 +3,33 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ["all",...new Set(items.map((item) => item.category))];
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category === 'all') {
+      setMenuItems(items);// If the filterItems function is run and finds the category of the string passed into the function as an argument is equal to 'all', it will display all menu items.
+      return;
+    }
+    const newItems = items.filter((item) => 
+      item.category === category)
+      setMenuItems(newItems)
+  }
+
+
+  return (
+    <section className="menu section">
+      <div className="title">
+        <h2>Menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Categories categories={categories} filterItems={filterItems}/>
+      <Menu items={menuItems} />
+    </section>
+  );
 }
 
 export default App;
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+// This is where we will render our buttons to switch between categories.
+const Categories = ({ categories, filterItems}) => {// {filterItems} is a prop passed in from the app component.  It must be passed into the component where it is returned in the App.js as well.
+  return (
+    <div className="btn-container">
+      {categories.map((category,index) => {
+        return <button type="button" className="filter-btn" key={index}
+        onClick={() => filterItems(category)}
+        >
+          {category}
+        </button>
+      } )}
+    </div>
+  );
 };
 
 export default Categories;
